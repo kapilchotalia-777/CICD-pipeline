a5dbc416df3ecc4d20c1b10e58f0ee0b24684c6c
kapilchotalia-777 <kapilchotalia777@gmail.com>
2024-08-09 12:23:11 +0530

    completed project

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Hello World! completed CICD-Pipeline');
 });
 
 const PORT = process.env.PORT || 8080;
